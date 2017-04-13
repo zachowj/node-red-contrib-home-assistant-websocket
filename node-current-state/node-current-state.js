364c5c23af993993cd621ed049577b87da8f1c94
Ara Yapejian <ayapejian@gmail.com>
2017-04-12 21:44:35 -0400

    Allow for halting a flow with current-state node

--- node-current-state/node-current-state.js
+++ node-current-state/node-current-state.js
@@ -4,7 +4,8 @@ const debug = require('debug')('ha-eventer:current-state');
 const _int = {
     getSettings: function(config) {
         return {
-            entity_id: config.entityid
+            entity_id: config.entityid,
+            haltIf:    config.haltif
         };
     },
     onInput: function(msg, node) {
@@ -16,8 +17,16 @@ const _int = {
             node.warn('Entity ID not set, no state to output');
         } else {
             const currentState = node.server.homeAssistant.states[entityId];
-            if (!currentState) { node.warn(`State not found for entity_id: ${entityId}`); }
-            else { node.send({ payload: currentState }); }
+            if (!currentState) {
+                node.warn(`State not found for entity_id: ${entityId}`);
+            // Stop the flow execution if 'halt if' setting is set and true
+            } else if (node.settings.haltIf) {
+                if (currentState.state === node.settings.haltIf) { node.log('Halting flow, current state === haltIf setting'); }
+                else { node.send({ payload: currentState }); }
+            // Else just send on
+            } else {
+                node.send({ payload: currentState });
+            }
         }
     }
 };
