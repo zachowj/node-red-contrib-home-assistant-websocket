31877df2a1f4311f51fd7388dd5d6594639c4867
Ara Yapejian <ayapejian@gmail.com>
2017-07-22 01:47:25 -0400

    cleanup

--- node-current-state/node-current-state.js
+++ node-current-state/node-current-state.js
@@ -1,11 +1,10 @@
 'use strict';
-const debug = require('debug')('home-assistant:current-state');
-
 const _int = {
     getSettings: function(config, node) {
         node.entity_id = config.entity_id;
         node.halt_if   = config.halt_if;
     },
+    /* eslint-disable consistent-return */
     onInput: function(msg, node) {
         const entity_id = (msg.payload && msg.payload.entity_id)
             ? msg.payload.entity_id
@@ -13,18 +12,19 @@ const _int = {
 
         if (!entity_id) {
             node.warn('Entity ID not set, cannot get current state');
-        } else {
-            const currentState = node.server.homeAssistant.states[entity_id];
-            if (!currentState) {
-                node.warn(`State not found for entity_id: ${entity_id}`);
-            // Stop the flow execution if 'halt if' setting is set and true
-            } else if (node.halt_if) {
-                if (currentState.state === node.halt_if) { debug(`Halting flow, current state of ${entity_id} === ${node.halt_if}`); }
-                else { node.send({ payload: currentState }); }
-            // Else just send on
-            } else {
-                node.send({ payload: currentState });
-            }
+            return null;
+        }
+
+        const currentState = node.server.homeAssistant.states[entity_id];
+        if (!currentState) {
+            node.warn(`entity could not be found in cache: ${entity_id}`);
+            return null;
+        }
+
+        if (node.halt_if && (currentState.state !== node.halt_if)) {
+            node.send({ payload: currentState });
+        } else if (!node.halt_if) {
+            node.send({ payload: currentState });
         }
     }
 };
