2a5d5df315bbce6f75e5c0db1ee44a24912cb5e4
Ara Yapejian <ayapejian@gmail.com>
2018-02-01 19:56:56 -0500

    fixed connection/re-connection issue and states cache problem after restart

--- nodes/config-server/config-server.js
+++ nodes/config-server/config-server.js
@@ -39,21 +39,6 @@ module.exports = function(RED) {
         constructor(nodeDefinition) {
             super(nodeDefinition, RED, nodeOptions);
 
-            if (this.nodeConfig.url && !this.homeAssistant) {
-                this.homeAssistant = new HomeAssistant({ baseUrl: this.nodeConfig.url, apiPass: this.nodeConfig.pass }, { startListening: true });
-                this.api    = this.homeAssistant.api;
-                this.events = this.homeAssistant.events;
-
-                // TODO: Run modify node-home-assistant to return error in testConnection()
-                // then swap out for real connection test, set startListening above to false
-                // const testConnection = () => Promise.resolve(true);
-                // testConnection()
-                //     .then(() => {
-                //         this.events.startListening();
-                //     })
-                //     .catch(err => this.error(err));
-            }
-
             this.RED.httpAdmin.get('/homeassistant/entities', httpHandlers.getEntities.bind(this));
             this.RED.httpAdmin.get('/homeassistant/states',   httpHandlers.getStates.bind(this));
             this.RED.httpAdmin.get('/homeassistant/services', httpHandlers.getServices.bind(this));
@@ -61,6 +46,55 @@ module.exports = function(RED) {
 
             const HTTP_STATIC_OPTS = { root: require('path').join(__dirname, '..', '/_static'), dotfiles: 'deny' };
             this.RED.httpAdmin.get('/homeassistant/static/*', function(req, res) { res.sendFile(req.params[0], HTTP_STATIC_OPTS) });
+
+            if (this.nodeConfig.url && !this.homeAssistant) {
+                this.homeAssistant = new HomeAssistant({ baseUrl: this.nodeConfig.url, apiPass: this.nodeConfig.pass }, { startListening: false });
+                this.api    = this.homeAssistant.api;
+                this.events = this.homeAssistant.events;
+
+                this.events.addListener('ha_events:close', this.onHaEventsClose.bind(this));
+                this.events.addListener('ha_events:open',  this.onHaEventsOpen.bind(this));
+                this.events.addListener('ha_events:error', this.onHaEventsError.bind(this));
+
+                this.homeAssistant.startListening()
+                    .catch(() => this.startListening());
+            }
+        }
+
+        // This simply tries to connected every 2 seconds, after the initial connection is successful
+        // reconnection attempts are handled by node-home-assistant.  This could use some love obviously
+        startListening() {
+            if (this.connectionAttempts) {
+                clearInterval(this.connectionAttempts);
+                this.connectionAttempts = null;
+            }
+
+            this.connectionAttempts = setInterval(() => {
+                this.homeAssistant.startListening()
+                    .then(() => {
+                        this.debug('Connected to home assistant');
+                        clearInterval(this.connectionAttempts);
+                        this.connectionAttempts = null;
+                    })
+                    .catch(err => this.error(`Home assistant connection failed with error: ${err.message}`));
+            }, 2000);
+        }
+
+        onHaEventsOpen() {
+            // This is another hack that should be set in node-home-assistant
+            // when the connection is first made the states cache needs to be refreshed
+            this.api.getStates(null, { forceRefresh: true });
+            this.api.getServices({ forceRefresh: true });
+            this.api.getEvents({ forceRefresh: true });
+            this.debug('config server event listener connected');
+        }
+
+        onHaEventsClose() {
+            this.debug('config server event listener closed');
+        }
+
+        onHaEventsError(err) {
+            this.debug(err);
         }
     }
 
