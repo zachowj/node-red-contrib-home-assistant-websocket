8a2d87c3022130d569424a8b81f93467b960cb32
Jason <jason@kermitdesign.com>
2018-09-20 02:44:42 -0700

    Added ability to filter event type within the 'events: all' node

--- nodes/events-all/events-all.js
+++ nodes/events-all/events-all.js
@@ -1,15 +1,24 @@
 const EventsNode = require('../../lib/events-node');
 
 module.exports = function(RED) {
+    const nodeOptions = {
+        config: {
+            event_type: {}
+        }
+    };
+
     class ServerEventsNode extends EventsNode {
         constructor(nodeDefinition) {
-            super(nodeDefinition, RED);
-            this.addEventClientListener({ event: 'ha_events:all', handler: this.onHaEventsAll.bind(this) });
+            super(nodeDefinition, RED, nodeOptions);
+            this.addEventClientListener({
+                event: 'ha_events:' + (this.nodeConfig.event_type || 'all'),
+                handler: this.onHaEventsAll.bind(this) }
+            );
         }
 
         onHaEventsAll(evt) {
             this.send({ event_type: evt.event_type, topic: evt.event_type, payload: evt });
-            this.status({fill: 'green', shape: 'dot', text: `${evt.event_type}`});
+            this.status({ fill: 'green', shape: 'dot', text: `${evt.event_type} at: ${this.getPrettyDate()}` });
         }
     }
 
