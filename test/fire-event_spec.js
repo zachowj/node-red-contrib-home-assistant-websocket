6ce93ad31019f2e7a07dd2a73c4fa96c1f8cb0a0
Jason <jason@kermitdesign.com>
2018-10-11 00:07:19 -0700

    Added fire-event tests

--- test/fire-event_spec.js
+++ test/fire-event_spec.js
@@ -21,4 +21,151 @@ describe('fire-event node', function() {
             done();
         });
     });
+
+    // Merge Context: Payload Data > Flow Data >  Global Data > Config Data ( payload data property always wins if provided )
+
+    it('should pass config data through', function(done) {
+        var flow = [
+            {
+                id: 'n1',
+                type: 'ha-fire-event',
+                name: 'fire-event',
+                event: 'test',
+                data: { foo: 'bar' },
+                wires: [['n2']]
+            },
+            { id: 'n2', type: 'helper' }
+        ];
+
+        helper.load(fireEvent, flow, function() {
+            var n1 = helper.getNode('n1');
+            var n2 = helper.getNode('n2');
+
+            n2.on('input', function(msg) {
+                msg.payload.should.have.property('event', 'test');
+                msg.payload.data.should.have.property('foo', 'bar');
+                done();
+            });
+
+            n1.receive({ payload: 'foo', topic: 'bar' });
+        });
+    });
+
+    it('should pass config event through but empty config data', function(done) {
+        var flow = [
+            {
+                id: 'n1',
+                type: 'ha-fire-event',
+                name: 'fire-event',
+                event: 'test',
+                wires: [['n2']]
+            },
+            { id: 'n2', type: 'helper' }
+        ];
+
+        helper.load(fireEvent, flow, function() {
+            var n1 = helper.getNode('n1');
+            var n2 = helper.getNode('n2');
+
+            n2.on('input', function(msg) {
+                msg.payload.should.have.property('event', 'test');
+                msg.payload.should.have.property('data', {});
+                done();
+            });
+
+            n1.receive({ payload: 'foo', topic: 'bar' });
+        });
+    });
+
+    it('should merge global data and pass through', function(done) {
+        var flow = [
+            {
+                id: 'n1',
+                type: 'ha-fire-event',
+                name: 'fire-event',
+                event: 'test',
+                data: { foo: 'bar' },
+                mergecontext: 'mergeName',
+                z: 'flowA',
+                wires: [['n2']]
+            },
+            { id: 'n2', type: 'helper', z: 'flowA' }
+        ];
+
+        helper.load(fireEvent, flow, function() {
+            var n1 = helper.getNode('n1');
+            var n2 = helper.getNode('n2');
+
+            n1.context().global.set('mergeName', { foo: 'baz' });
+
+            n2.on('input', function(msg) {
+                msg.payload.data.should.have.property('foo', 'baz');
+                done();
+            });
+
+            n1.receive({ payload: 'foo', topic: 'bar' });
+        });
+    });
+
+    it('should merge flow data and pass through', function(done) {
+        var flow = [
+            {
+                id: 'n1',
+                type: 'ha-fire-event',
+                name: 'fire-event',
+                event: 'test',
+                data: { foo: 'bar' },
+                mergecontext: 'mergeName',
+                z: 'flowA',
+                wires: [['n2']]
+            },
+            { id: 'n2', type: 'helper', z: 'flowA' }
+        ];
+
+        helper.load(fireEvent, flow, function() {
+            var n1 = helper.getNode('n1');
+            var n2 = helper.getNode('n2');
+
+            n1.context().global.set('mergeName', { foo: 'baz' });
+            n1.context().flow.set('mergeName', { foo: 'zxc' });
+
+            n2.on('input', function(msg) {
+                msg.payload.data.should.have.property('foo', 'zxc');
+                done();
+            });
+
+            n1.receive({ payload: 'foo', topic: 'bar' });
+        });
+    });
+
+    it('should merge payload data and pass through', function(done) {
+        var flow = [
+            {
+                id: 'n1',
+                type: 'ha-fire-event',
+                name: 'fire-event',
+                event: 'test',
+                data: { foo: 'bar' },
+                mergecontext: 'mergeName',
+                z: 'flowA',
+                wires: [['n2']]
+            },
+            { id: 'n2', type: 'helper', z: 'flowA' }
+        ];
+
+        helper.load(fireEvent, flow, function() {
+            var n1 = helper.getNode('n1');
+            var n2 = helper.getNode('n2');
+
+            n1.context().global.set('mergeName', { foo: 'baz' });
+            n1.context().flow.set('mergeName', { foo: 'zxc' });
+
+            n2.on('input', function(msg) {
+                msg.payload.data.should.have.property('foo', 'toto');
+                done();
+            });
+
+            n1.receive({ payload: { data: { foo: 'toto' } }, topic: 'bar' });
+        });
+    });
 });
