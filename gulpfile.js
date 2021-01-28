44dc3bf624b897f9c9c46f822c744f9b342191ed
Jason <37859597+zachowj@users.noreply.github.com>
2021-01-28 11:41:05 -0800

    build: fix the file watch wildcards

--- gulpfile.js
+++ gulpfile.js
@@ -327,7 +327,7 @@ module.exports = {
         runNodemonAndBrowserSync,
         function watcher(done) {
             watch(
-                ['lib/ui/*', 'nodes/*/ui-*', 'docs/node/*.md'],
+                ['lib/ui/*', 'nodes/**/ui-*', 'docs/node/*.md'],
                 series(
                     'cleanFiles',
                     'buildEditorFiles',
@@ -336,7 +336,7 @@ module.exports = {
             );
             // only server side files modified restart node-red only
             watch(
-                ['nodes/*/*.js', '!nodes/*/ui-*.js', 'lib/*/*.js'],
+                ['nodes/**/*.js', '!nodes/**/ui-*.js', 'lib/**/*.js'],
                 restartNodemon
             );
             done();
