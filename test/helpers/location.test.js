1556d4f907a2d1769f7dfde419747c9c19a0d834
Jason <37859597+zachowj@users.noreply.github.com>
2021-06-07 01:54:05 -0700

    tests: Fix test for typescript files

---
+++ test/helpers/location.test.js
@@ -0,0 +1,64 @@
+const expect = require('chai').expect;
+
+const { getLocationData, getZoneData } = require('../../src/helpers/location');
+
+describe('location', function () {
+    describe('getLocationData', function () {
+        it('should return coords', function () {
+            const goodEntity = {
+                attributes: {
+                    latitude: 1,
+                    longitude: 1,
+                },
+            };
+            const location = getLocationData(goodEntity);
+            expect(location).to.have.property('latitude');
+            expect(location).to.have.property('longitude');
+        });
+        it('should return false for invalid latitude', function () {
+            const badLatitude = {
+                attributes: {
+                    latitude: 'abc',
+                    longitude: 1,
+                },
+            };
+            const location = getLocationData(badLatitude);
+            expect(location).to.be.false;
+        });
+        it('should return false for invalid longitude', function () {
+            const badLongitude = {
+                attributes: {
+                    latitude: 1,
+                    longitude: false,
+                },
+            };
+            const location = getLocationData(badLongitude);
+            expect(location).to.be.false;
+        });
+    });
+    describe('getZoneData', function () {
+        it('should return coords and radius', function () {
+            const goodZone = {
+                attributes: {
+                    latitude: 1,
+                    longitude: 1,
+                    radius: 100,
+                },
+            };
+            const location = getZoneData(goodZone);
+            expect(location).to.have.property('latitude');
+            expect(location).to.have.property('longitude');
+            expect(location).to.have.property('radius');
+        });
+        it('should return false for invalid radius', function () {
+            const badRadius = {
+                attributes: {
+                    latitude: 1,
+                    longitude: 1,
+                },
+            };
+            const location = getZoneData(badRadius);
+            expect(location).to.be.false;
+        });
+    });
+});
