b641ec96940776e00eb5e5b4bd02455c2c418bb1
Jason <37859597+zachowj@users.noreply.github.com>
2021-12-09 23:18:47 -0800

    chore: linting fix

--- src/controllers/CurrentState.js
+++ src/controllers/CurrentState.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 const BaseNode = require('./BaseNode');
-const { renderTemplate } = require('../helpers/renderTemplate');
 const { getTimeInMilliseconds } = require('../helpers/utils');
+const { renderTemplate } = require('../helpers/renderTemplate');
 
 const nodeOptions = {
     config: {
