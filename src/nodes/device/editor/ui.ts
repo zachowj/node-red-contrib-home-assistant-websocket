2d0e37f573f6b6a70c53aab6fd694a9489021813
Jason <37859597+zachowj@users.noreply.github.com>
2022-01-30 09:20:26 -0800

    fix(device): Add missing integer type for the device capabilities
    fix(device): Add missing boolean type for the device capabilities
    
    Closes #565

--- src/nodes/device/editor/ui.ts
+++ src/nodes/device/editor/ui.ts
@@ -4,59 +4,139 @@ export function createDeviceExtraFields(
     fields: any[] = [],
     capabilities: any[] = []
 ) {
-    let elements = '';
+    const elements: JQuery[] = [];
     fields.forEach((field) => {
         const selectedCapabilities = capabilities.find(
             (i) => i.name === field.name
         );
-        let element;
         switch (field.type) {
-            case 'float':
-                element = createDeviceFloat(field, selectedCapabilities);
-                break;
-            case 'positive_time_period_dict':
-                element = createDeviceDuration(field, selectedCapabilities);
-                break;
-            case 'select': {
-                element = createDeviceSelect(field, selectedCapabilities);
+            case 'boolean': {
+                const element = createDeviceBoolean(
+                    field,
+                    selectedCapabilities
+                );
+                elements.push($(element));
                 break;
             }
-            case 'string':
-                element = createDeviceString(field, selectedCapabilities);
+            case 'float': {
+                const element = createDeviceFloat(field, selectedCapabilities);
+                elements.push($(element));
                 break;
+            }
+            case 'integer': {
+                const element = createDeviceInteger(
+                    field,
+                    selectedCapabilities
+                );
+                elements.push(element);
+                break;
+            }
+            case 'positive_time_period_dict': {
+                const element = createDeviceDuration(
+                    field,
+                    selectedCapabilities
+                );
+                elements.push($(element));
+                break;
+            }
+            case 'select': {
+                const element = createDeviceSelect(field, selectedCapabilities);
+                elements.push($(element));
+                break;
+            }
+            case 'string': {
+                const element = createDeviceString(field, selectedCapabilities);
+                elements.push($(element));
+                break;
+            }
             default: {
                 const text = haI18n('ha-device.error.unknown_field_type', {
                     type: field.type,
                 });
-                element = createDeviceError(text, { type: field.type });
+                const element = createDeviceError(text, {
+                    type: field.type,
+                });
+                elements.push($(element));
             }
         }
-        if (element) elements += element;
     });
 
     return elements;
 }
 
+function createDeviceBoolean(field: any, store: any) {
+    const id = `deviceExtra-${field.name}`;
+    const value = store ? store.value : '';
+    const html = `
+        <label for="${id}">${field.name}</label>
+        <input type="checkbox" id="${id}" style="width: auto" value="${value}"/>        
+        `;
+
+    return wrapWithRow(html);
+}
+
 function createDeviceFloat(field: any, store: any) {
     const id = `deviceExtra-${field.name}`;
     const value = store ? store.value : '';
-    const icon = ['above', 'below'].includes(field.name)
-        ? `<i class="fa fa-angle-double-${
-              field.name === 'above' ? 'up' : 'down'
-          }"></i> `
-        : '';
     let html = `
-        <label for="${id}">
-            ${icon}${field.name}
-        </label>
+        <label for="${id}">${field.name}</label>
         <input type="text" id="${id}" style="width: 35%" value="${value}"/>        
         `;
     if (field.description && field.description.suffix) {
         html += `<span>${field.description.suffix}</span>`;
     }
 
-    return wrapWithRow(html, ['deviceExtra']);
+    return wrapWithRow(html);
 }
+
+function createDeviceInteger(field: any, store: any) {
+    const id = `deviceExtra-${field.name}`;
+    const value = store ? store.value : '';
+    const $elements = $(`
+        <label for="${id}">${field.name}</label>
+        <div class="range-row">
+            <input type="checkbox" />
+            <input type="range" id="${id}" max="${field.valueMax}" min="${
+        field.valueMin ?? 0
+    }" value="${value}" />
+            <input type="text" value="${value}" />
+        </div>
+        `);
+    const $checkbox = $elements.find('input[type="checkbox"]');
+    const $range = $elements.find('input[type="range"]');
+    const $text = $elements.find('input[type="text"]');
+    if (field.optional === false || store?.value !== undefined) {
+        $checkbox.prop('checked', true);
+    }
+    if (field.optional === false) {
+        $checkbox.hide();
+    }
+    $checkbox
+        .on('change', () => {
+            $range
+                .prop('disabled', !$checkbox.prop('checked'))
+                .trigger('change');
+            $text.prop('disabled', !$checkbox.prop('checked'));
+        })
+        .trigger('change');
+    $range
+        .on('change input', () => {
+            $text.val($range.val());
+        })
+        .trigger('change');
+    $text.on('keyup change', () => {
+        const val = $text.val() as string;
+        if (val.length > 0) {
+            const num = Number(val);
+            if (num < field.valueMin) $text.val(field.valueMin);
+            if (num > field.valueMax) $text.val(field.valueMax);
+            $range.val($text.val());
+        }
+    });
+
+    return wrapWithRow($elements);
+}
+
 function createDeviceDuration(field: Record<string, any>, store: any) {
     const id = `deviceExtra-${field.name}`;
     const value = store ? store.value : '';
@@ -82,7 +162,7 @@ function createDeviceDuration(field: Record<string, any>, store: any) {
     }</option>
             <option value="hours" ${selected('hours')}>${i18n.hours}</option>
         </select>`;
-    return wrapWithRow(html, ['deviceExtra']);
+    return wrapWithRow(html);
 }
 
 function createDeviceSelect(
@@ -105,7 +185,7 @@ function createDeviceSelect(
         ${options.join('')}
         </select>        
         `;
-    return wrapWithRow(html, ['deviceExtra']);
+    return wrapWithRow(html);
 }
 
 function createDeviceString(
@@ -118,17 +198,24 @@ function createDeviceString(
         <label for="${id}">${field.name}</label>
         <input type="text" id="${id}" style="width: 70%" value="${value}"/>
         `;
-    return wrapWithRow(html, ['deviceExtra']);
+    return wrapWithRow(html);
 }
 
 function createDeviceError(text: string, opts: any = {}) {
     const html = `<label class="error"><i class="fa fa-exclamation-triangle"></i> Error</label>${text} -- <a href="https://github.com/zachowj/node-red-contrib-home-assistant-websocket/issues/new?title=[Device Node] Unknown extra type: ${
         opts.type
     }" target="_blank"> ${haI18n('ha-device.error.report')}</a>`;
-    return wrapWithRow(html, ['deviceExtra']);
+    return wrapWithRow(html);
 }
 
-function wrapWithRow(ele: string, cls: string[] = []) {
+function wrapWithRow(ele: JQuery, cls?: string[]): JQuery;
+function wrapWithRow(ele: string, cls?: string[]): string;
+function wrapWithRow(ele: unknown, cls: string[] = ['deviceExtra']): unknown {
+    if (ele instanceof $) {
+        return $(`<div class="form-row ${cls.join(' ')}"></div>`).append(
+            ele as JQuery
+        );
+    }
     return `<div class="form-row ${cls.join(' ')}">${ele}</div>`;
 }
 
@@ -137,6 +224,13 @@ export function getCapabilities(capabilities: any[] = []) {
         const id = `deviceExtra-${item.name}`;
         const value = $(`#${id}`).val() as string;
         switch (item.type) {
+            case 'boolean':
+                acc.push({
+                    name: item.name,
+                    type: item.type,
+                    value: $(`#${id}`).is(':checked'),
+                });
+                break;
             case 'float':
             case 'positive_time_period_dict':
                 if (value && value.length && !isNaN(Number(value))) {
@@ -152,6 +246,21 @@ export function getCapabilities(capabilities: any[] = []) {
                     acc.push(cap);
                 }
                 break;
+            case 'integer': {
+                const $sel = $(`#${id}`);
+                const checked = $sel
+                    .siblings('input[type="checkbox"]')
+                    .prop('checked');
+                if (checked && value && value.length && !isNaN(Number(value))) {
+                    const cap = {
+                        name: item.name,
+                        type: item.type,
+                        value: Number(value),
+                    };
+                    acc.push(cap);
+                }
+                break;
+            }
             case 'select':
                 if (value && value.length) {
                     acc.push({
