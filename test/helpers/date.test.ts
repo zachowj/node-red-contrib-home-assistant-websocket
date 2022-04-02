4d325f8732b0593154ae1ca5c864b50623b56485
Jason <37859597+zachowj@users.noreply.github.com>
2022-04-01 22:42:53 -0700

    test: Add condition for node 12 not fully supporting Intl.DateTimeFormatOptions

--- test/helpers/date.test.ts
+++ test/helpers/date.test.ts
@@ -3,6 +3,11 @@ import { expect } from 'chai';
 import { formatDate } from '../../src/helpers/date';
 
 describe('formatDate', function () {
+    let nodeVersion: string;
+    before(function () {
+        nodeVersion = process.version.substring(1, 3);
+    });
+
     it('should format date', function () {
         const date = new Date(2012, 0, 12, 12, 12, 12);
         const formattedDate = formatDate({
@@ -12,6 +17,8 @@ describe('formatDate', function () {
         expect(formattedDate).to.equal('Jan 12, 12:12 PM');
     });
     it('should format date using en-GB as locale', function () {
+        // node 12 does not support en-GB locale
+        if (nodeVersion === '12') this.skip();
         const date = new Date(2012, 0, 12, 16, 12, 12);
         const formattedDate = formatDate({
             locale: 'en-GB',
@@ -20,6 +27,8 @@ describe('formatDate', function () {
         expect(formattedDate).to.equal('12 Jan, 16:12');
     });
     it('should format date to include all options', function () {
+        // node 12 does not support Intl.DateTimeFormatOptions fractionalSecondDigits
+        if (nodeVersion === '12') this.skip();
         const date = new Date(2012, 0, 12, 12, 12, 12, 123);
         const formattedDate = formatDate({
             locale: 'en-US',
@@ -31,7 +40,7 @@ describe('formatDate', function () {
                 hour: '2-digit',
                 minute: '2-digit',
                 second: '2-digit',
-                fractionalSecondDigits: '3',
+                fractionalSecondDigits: 3,
             },
         });
         expect(formattedDate).to.equal('01/12/2012, 12:12:12.123 PM');
