867289a6bfb3bdc77adfa1756adc5489f30e6983
Jason <37859597+zachowj@users.noreply.github.com>
2023-07-11 23:05:39 -0700

    fix(sentence): Catch error from custom outputs

--- src/nodes/sentence/SentenceController.ts
+++ src/nodes/sentence/SentenceController.ts
@@ -3,7 +3,6 @@ import OutputController, {
 } from '../../common/controllers/OutputController';
 import { IntegrationEvent } from '../../common/integration/Integration';
 import { NodeMessage } from '../../types/nodes';
-import { EntityConfigNode } from '../entity-config';
 import { SentenceNode } from '.';
 
 interface SentenceResponse {
@@ -13,8 +12,6 @@ interface SentenceResponse {
 type SentenceNodeOptions = OutputControllerOptions<SentenceNode>;
 
 export default class SentenseController extends OutputController<SentenceNode> {
-    #entityConfigNode?: EntityConfigNode;
-
     constructor(props: SentenceNodeOptions) {
         super(props);
 
@@ -27,10 +24,16 @@ export default class SentenseController extends OutputController<SentenceNode> {
     #onReceivedMessage(data: SentenceResponse) {
         this.status.setSuccess('home-assistant.status.triggered');
         const message: NodeMessage = {};
-        this.setCustomOutputs(this.node.config.outputProperties, message, {
-            config: this.node.config,
-            triggerId: data.sentence,
-        });
+        try {
+            this.setCustomOutputs(this.node.config.outputProperties, message, {
+                config: this.node.config,
+                triggerId: data.sentence,
+            });
+        } catch (e) {
+            this.node.error(e);
+            this.status.setFailed('error');
+            return;
+        }
         this.node.send(message);
     }
 }
