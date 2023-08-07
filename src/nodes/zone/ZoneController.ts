16bd3eb25fa5734b3b23d33ca8bc3f34521de0d9
Jason <37859597+zachowj@users.noreply.github.com>
2023-08-06 19:57:18 -0700

    refactor(zone): Convert controller to Typescript
    refactor(zone): Change expose as to use entity config
    
    BREAKING CHANGE: Expose as won't work until manually converted in the Node-RED UI

---
+++ src/nodes/zone/ZoneController.ts
@@ -0,0 +1,88 @@
+import cloneDeep from 'lodash.clonedeep';
+
+import ExposeAsController from '../../common/controllers/EposeAsController';
+import { HassEntity, HassStateChangedEvent } from '../../types/home-assistant';
+import { ZoneNode } from '.';
+import { getLocationData, getZoneData, inZone } from './helpers';
+
+export type locationData = {
+    latitude: number;
+    longitude: number;
+    radius?: number;
+};
+
+enum ZoneEvent {
+    Enter = 'enter',
+    EnterOrLeave = 'enter_leave',
+    Leave = 'leave',
+}
+
+export default class Zone extends ExposeAsController<ZoneNode> {
+    #getValidZones(fromState: HassEntity, toState: HassEntity) {
+        const config = this.node.config;
+        const fromLocationData = getLocationData(fromState);
+        const toLocationData = getLocationData(toState);
+        if (!fromLocationData || !toLocationData) return [];
+        const zones = this.#getZones();
+        const validZones = zones.filter((zone) => {
+            const zoneData = getZoneData(zone);
+            if (!zoneData) return false;
+            const fromMatch = inZone(fromLocationData, zoneData);
+            const toMatch = inZone(toLocationData, zoneData);
+
+            return (
+                (config.event === ZoneEvent.Enter && !fromMatch && toMatch) ||
+                (config.event === ZoneEvent.Leave && fromMatch && !toMatch) ||
+                (config.event === ZoneEvent.EnterOrLeave &&
+                    fromMatch !== toMatch)
+            );
+        });
+
+        return validZones;
+    }
+
+    #getZones() {
+        const entities = this.homeAssistant.websocket.getStates();
+        const zones: HassEntity[] = [];
+        for (const entityId in entities) {
+            if (this.node.config.zones.includes(entityId)) {
+                zones.push(entities[entityId] as HassEntity);
+            }
+        }
+
+        return zones;
+    }
+
+    public onStateChanged(evt: HassStateChangedEvent) {
+        if (
+            this.isEnabled === false ||
+            !this.homeAssistant.isHomeAssistantRunning
+        ) {
+            return;
+        }
+
+        const { entity_id: entityId, event } = cloneDeep(evt);
+
+        if (!event.old_state || !event.new_state) return;
+
+        const zones = this.#getValidZones(event.old_state, event.new_state);
+
+        if (!zones.length) return;
+
+        event.new_state.timeSinceChangedMs =
+            Date.now() - new Date(event.new_state.last_changed).getTime();
+
+        const msg = {
+            topic: entityId,
+            payload: event.new_state.state,
+            data: event,
+            zones,
+        };
+        const statusMessage = `${entityId} ${this.node.config.event} ${zones
+            .map((z) => z.entity_id)
+            .join(',')}`;
+
+        this.status.setSuccess(statusMessage);
+        this.node.send(msg);
+    }
+}
