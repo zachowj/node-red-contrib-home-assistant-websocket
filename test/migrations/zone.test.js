c0db77fd85ee81f6a887978bb1e91443121ab6e5
Jason <37859597+zachowj@users.noreply.github.com>
2023-08-06 20:11:41 -0700

    test: Fix tests for zone node

--- test/migrations/zone.test.js
+++ test/migrations/zone.test.js
@@ -16,6 +16,13 @@ const VERSION_0 = {
     ...VERSION_UNDEFINED,
     version: 0,
 };
+const VERSION_1 = {
+    ...VERSION_0,
+    version: 1,
+    exposeAsEntityConfig: '',
+    exposeToHomeAssistant: undefined,
+    haConfig: undefined,
+};
 
 describe('Migrations - Zone Node', function () {
     describe('Version 0', function () {
@@ -25,8 +32,15 @@ describe('Migrations - Zone Node', function () {
             expect(migratedSchema).to.eql(VERSION_0);
         });
     });
+    describe('Version 1', function () {
+        it('should update version 0 to version 1', function () {
+            const migrate = migrations.find((m) => m.version === 1);
+            const migratedSchema = migrate.up(VERSION_0);
+            expect(migratedSchema).to.eql(VERSION_1);
+        });
+    });
     it('should update an undefined version to current version', function () {
         const migratedSchema = migrate(VERSION_UNDEFINED);
-        expect(migratedSchema).to.eql(VERSION_0);
+        expect(migratedSchema).to.eql(VERSION_1);
     });
 });
