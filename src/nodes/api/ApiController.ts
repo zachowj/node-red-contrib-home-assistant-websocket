d2376c25d7aa04f6c0ee27234822cd0107180878
Jason <37859597+zachowj@users.noreply.github.com>
2023-11-18 11:44:30 -0800

    refactor: Convert api node to typescript

---
+++ src/nodes/api/ApiController.ts
@@ -0,0 +1,116 @@
+import InputOutputController, {
+    InputProperties,
+} from '../../common/controllers/InputOutputController';
+import InputError from '../../common/errors/InputError';
+import NoConnectionError from '../../common/errors/NoConnectionError';
+import { DataSource } from '../../common/services/InputService';
+import { TypedInputTypes } from '../../const';
+import { generateRenderTemplate } from '../../helpers/mustache';
+import { ApiMethod, ApiNode, ApiNodeProperties, ApiProtocol } from '.';
+
+export default class ApiController extends InputOutputController<
+    ApiNode,
+    ApiNodeProperties
+> {
+    protected async onInput({
+        message,
+        parsedMessage,
+        send,
+        done,
+    }: InputProperties) {
+        if (!this.homeAssistant.websocket.isConnected) {
+            throw new NoConnectionError();
+        }
+
+        const renderTemplate = generateRenderTemplate(
+            message,
+            this.node.context(),
+            this.homeAssistant.websocket.getStates()
+        );
+
+        let data;
+        if (parsedMessage.data.source === DataSource.Message) {
+            data = parsedMessage.data.value;
+        } else if (parsedMessage.data.value?.length > 0) {
+            if (parsedMessage.dataType.value === TypedInputTypes.JSONata) {
+                data = await this.jsonataService.evaluate(
+                    parsedMessage.data.value,
+                    {
+                        message,
+                    }
+                );
+            } else {
+                data = JSON.parse(
+                    renderTemplate(
+                        typeof parsedMessage.data.value === 'object'
+                            ? JSON.stringify(parsedMessage.data.value)
+                            : parsedMessage.data.value
+                    )
+                );
+            }
+        }
+
+        let results;
+        if (parsedMessage.protocol.value === ApiProtocol.Http) {
+            const path = renderTemplate(parsedMessage.path.value).replace(
+                /^\/(?:api\/)?/,
+                ''
+            );
+
+            if (!path) {
+                throw new InputError('ha-api.error.requires_path');
+            }
+            if (
+                !Object.values(ApiMethod).includes(parsedMessage.method.value)
+            ) {
+                throw new InputError([
+                    'ha-api.error.invalid_method',
+                    { method: parsedMessage.method.value },
+                ]);
+            }
+
+            const method = parsedMessage.method.value;
+            this.debugToClient({ method, path, data });
+
+            if (method === ApiMethod.Get) {
+                results = await this.homeAssistant.http.get(
+                    path,
+                    data,
+                    parsedMessage.responseType.value
+                );
+            } else {
+                results = await this.homeAssistant.http.post(
+                    path,
+                    data,
+                    parsedMessage.responseType.value
+                );
+            }
+        } else {
+            if (!('type' in data)) {
+                throw new InputError('ha-api.error.requires_type');
+            }
+
+            this.debugToClient(JSON.stringify(data));
+
+            this.status.setSending();
+            results = this.homeAssistant.websocket.send(data);
+        }
+
+        await this.setCustomOutputs(
+            parsedMessage.outputProperties.value,
+            message,
+            {
+                results,
+                config: this.node.config,
+            }
+        );
+
+        this.status.setSuccess([
+            'ha-api.status.method_called',
+            { method: parsedMessage.protocol.value },
+        ]);
+
+        send(message);
+        done();
+    }
+}
--- src/nodes/api/index.ts
+++ src/nodes/api/index.ts
@@ -1,20 +1,128 @@
+import Joi from 'joi';
+
+import { createControllerDependencies } from '../../common/controllers/helpers';
+import InputService, { NodeInputs } from '../../common/services/InputService';
+import Status from '../../common/status/Status';
+import { TypedInputTypes } from '../../const';
 import { RED } from '../../globals';
 import { migrate } from '../../helpers/migrate';
-import { Status } from '../../helpers/status';
-import { checkValidServerConfig } from '../../helpers/utils';
-import { BaseNode, NodeProperties } from '../../types/nodes';
-import Api from './controller';
+import { getServerConfigNode } from '../../helpers/node';
+import { getHomeAssistant } from '../../homeAssistant';
+import {
+    BaseNode,
+    BaseNodeProperties,
+    OutputProperty,
+} from '../../types/nodes';
+import ApiController from './ApiController';
 
-export default function apiNode(this: BaseNode, config: NodeProperties) {
+export interface ApiNodeProperties extends BaseNodeProperties {
+    protocol: string;
+    method: string;
+    path: string;
+    data: string;
+    dataType: TypedInputTypes;
+    responseType: string;
+    outputProperties: OutputProperty[];
+    location?: string;
+    locationType?: TypedInputTypes;
+}
+
+export interface ApiNode extends BaseNode {
+    config: ApiNodeProperties;
+}
+
+export enum ApiMethod {
+    Get = 'get',
+    Post = 'post',
+}
+
+export enum ApiProtocol {
+    Http = 'http',
+    Websocket = 'websocket',
+}
+
+const inputs: NodeInputs = {
+    protocol: {
+        messageProp: 'payload.protocol',
+        configProp: 'protocol',
+    },
+    method: {
+        messageProp: 'payload.method',
+        configProp: 'method',
+    },
+    path: {
+        messageProp: 'payload.path',
+        configProp: 'path',
+    },
+    data: {
+        messageProp: 'payload.data',
+        configProp: 'data',
+    },
+    dataType: {
+        messageProp: 'payload.dataType',
+        configProp: 'dataType',
+    },
+    location: {
+        messageProp: 'payload.location',
+        configProp: 'location',
+    },
+    locationType: {
+        messageProp: 'payload.locationType',
+        configProp: 'locationType',
+    },
+    responseType: {
+        messageProp: 'payload.responseType',
+        configProp: 'responseType',
+    },
+    outputProperties: {
+        messageProp: 'payload.outputProperties',
+        configProp: 'outputProperties',
+    },
+};
+
+const inputSchema: Joi.ObjectSchema = Joi.object({
+    protocol: Joi.string().valid(...Object.values(ApiProtocol)),
+    method: Joi.string().valid(...Object.values(ApiMethod)),
+    path: Joi.string().allow(''),
+    dataType: Joi.string().valid(TypedInputTypes.JSON, TypedInputTypes.JSONata),
+    data: Joi.optional(),
+    location: Joi.string(),
+    locationType: Joi.string().valid(
+        TypedInputTypes.Message,
+        TypedInputTypes.Flow,
+        TypedInputTypes.Global,
+        TypedInputTypes.None
+    ),
+    responseType: Joi.string().valid('json', 'text', 'arraybuffer'),
+    outputProperties: Joi.array().items(),
+});
+
+export default function apiNode(
+    this: ApiNode,
+    config: ApiNodeProperties
+): void {
     RED.nodes.createNode(this, config);
 
     this.config = migrate(config);
-    checkValidServerConfig(this, this.config.server);
-    const status = new Status(this);
-    this.controller = new Api({
+    const serverConfigNode = getServerConfigNode(this.config.server);
+    const homeAssistant = getHomeAssistant(serverConfigNode);
+
+    const status = new Status({
+        config: serverConfigNode.config,
         node: this,
-        config: this.config,
-        RED,
+    });
+    const inputService = new InputService<ApiNodeProperties>({
+        inputs,
+        nodeConfig: this.config,
+        schema: inputSchema,
+    });
+    const controllerDeps = createControllerDependencies(this, homeAssistant);
+
+    // eslint-disable-next-line no-new
+    new ApiController({
+        inputService,
+        node: this,
         status,
+        ...controllerDeps,
     });
 }
