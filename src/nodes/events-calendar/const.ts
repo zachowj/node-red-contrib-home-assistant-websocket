3cfec64dc7b1b25b74a37c63f7e90582008bfc61
Nathan Brittain <git@enjibby.dev>
2023-12-31 11:23:00 +0000

    Remove Unnecessary constraint interface definition

--- src/nodes/events-calendar/const.ts
+++ src/nodes/events-calendar/const.ts
@@ -1,26 +1,3 @@
-import { ComparatorType, TypedInputTypes } from '../../const';
-
-export enum TargetType {
-    ThisEntity = 'this_entity',
-    EntityId = 'entity_id',
-}
-
-export enum PropertyType {
-    CurrentState = 'current_state',
-    PreviousState = 'previous_state',
-    Property = 'property',
-}
-
-export interface Constraint {
-    targetType: TargetType;
-    targetValue: string;
-    propertyType: PropertyType;
-    propertyValue: string;
-    comparatorType: ComparatorType;
-    comparatorValueDatatype: TypedInputTypes;
-    comparatorValue: string;
-}
-
 // Define type aliases
 type WithDateTime = {
     dateTime: string;
